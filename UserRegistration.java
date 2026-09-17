8921ff0a055cab7909ea8aebf11b53df36fce65b
Ntshuxe <valentiamaluleke08@gmail.com>
2026-09-17 13:04:42 +0200

    Updated my UserRegistration.java by adding loops
    Added looping to prompt the user to retry if they enter incorrect input

--- UserRegistration.java
+++ UserRegistration.java
@@ -65,45 +65,55 @@ public class UserRegistration{
         System.out.print("Please enter your first name: ");
         firstName = input.nextLine();
         
-        if(firstName.isEmpty()){
+        while(firstName.isEmpty()){
             System.out.println("Firs name cannot be empty");
-        }else{
+            System.out.print("Please enter your first name: ");
+            firstName = input.nextLine();
+        }
         System.out.print("Please enter your last name: ");
         lastName = input.nextLine();
         
-         if(lastName.isEmpty()){
+         while(lastName.isEmpty()){
             System.out.println("Last name cannot be empty");
-        }else{
+            System.out.print("Please enter your last name: ");
+            lastName = input.nextLine();
+        }
         
         Login login = new Login(firstName, lastName);
 
         System.out.print("Enter your username (less than 5 characters and must contain an underscore): ");
         userName = input.nextLine();
         
-        if(!login.checkUserName(userName)){
+        while(!login.checkUserName(userName)){
             System.out.println("Username is not correctly formatted, please ensure that your username contains an underscore and is no more than five characters in length.");
-        }else{
+            System.out.print("Enter your username (less than 5 characters and must contain an underscore): ");
+            userName = input.nextLine();
+        }
 
         System.out.print("Create a password (at least 8 characters, 1 capital letter, 1 number, 1 special character): ");
         password = input.nextLine();
         
-        if(!login.checkPasswordComplexity(password)){
+        while(!login.checkPasswordComplexity(password)){
             System.out.println("Password is not correctly formatted; please ensure that the password contains at least eight characters, a capital letter, a number, and a special character.");
-        }else{
+            System.out.print("Create a password (at least 8 characters, 1 capital letter, 1 number, 1 special character): ");
+            password = input.nextLine();
+        }
 
         System.out.print("Please enter cellphone number with international country code: ");
         cellPhoneNumber = input.nextLine();
         
-        if(!login.checkCellPhoneNumber(cellPhoneNumber)){
+        while(!login.checkCellPhoneNumber(cellPhoneNumber)){
             System.out.println("Cell phone number incorrectly formatted or does not contain international code, please correct the number and try again");  
-        }else{
+            System.out.print("Please enter cellphone number with international country code: ");
+            cellPhoneNumber = input.nextLine();
+        }
         
         // Register and show registration status
         String regStatus = login.registerUser(userName, password, cellPhoneNumber, firstName, lastName);
         System.out.println(regStatus);
         
-        // If registration is successfull allow login
-        if (regStatus.equals("Registration successful.")) {
+        // If registration is successfull allow login if not the loop will prompt the user to retry until successful
+        while (regStatus.equals("Registration successful.")) {
             System.out.println("******User Login******");
 
             System.out.print("Enter the username you created: ");
@@ -114,7 +124,12 @@ public class UserRegistration{
 
             String loginStatus = login.returnLoginStatus(loginUser, loginPass);
             System.out.println(loginStatus);
-
+            
+        if(loginStatus.contains("Welcome")){
+            break;
+        }
+        
+            System.out.println("Login failed, please try again");
         }
 
         input.close();
@@ -122,11 +137,11 @@ public class UserRegistration{
  
      }
     }
-        }
-         }
-        }
-    }
-}
+        
+         
+        
+    
+
    
   
  
